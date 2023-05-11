bec0cdfb13249d2e79155282f24eb55a8ce83b3c
Sycrosity <72102935+Sycrosity@users.noreply.github.com>
2023-05-11 23:43:27 +0100

    fix derive

--- crates/statik_derive/src/lib.rs
+++ crates/statik_derive/src/lib.rs
@@ -1,14 +1,22 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
+#![allow(unused)]
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+mod packet;
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+#[macro_use]
+extern crate quote;
+#[macro_use]
+extern crate syn;
+
+extern crate proc_macro;
+extern crate proc_macro2;
+
+use proc_macro::TokenStream;
+use syn::{DeriveInput, GenericParam, Generics};
+
+#[proc_macro_derive(Packet, attributes(packet_id))]
+pub fn derive_serialize(input: TokenStream) -> TokenStream {
+    let mut input = parse_macro_input!(input as syn::DeriveInput);
+    packet::expand_derive_packet(&mut input)
+        .unwrap_or_else(syn::Error::into_compile_error)
+        .into()
 }
