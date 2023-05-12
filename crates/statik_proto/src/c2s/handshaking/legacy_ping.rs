480a76847bd55247821fff8b1d08217ec9c3c583
Sycrosity <72102935+Sycrosity@users.noreply.github.com>
2023-05-12 18:57:36 +0100

    removed incorrect lecacy_ping impls

--- crates/statik_proto/src/c2s/handshaking/legacy_ping.rs
+++ crates/statik_proto/src/c2s/handshaking/legacy_ping.rs
@@ -7,27 +7,3 @@ pub struct C2SLegacyPing {
     ///always 1 (0x01).
     pub payload: u8,
 }
-
-impl Packet for C2SLegacyPing {
-    fn id(&self) -> VarInt {
-        VarInt(0x0F)
-    }
-
-    fn length(&self) -> VarInt {
-        VarInt(2)
-    }
-}
-
-impl Encode for C2SLegacyPing {
-    fn encode(&self, buffer: &mut dyn std::io::Write) -> anyhow::Result<()> {
-        self.payload.encode(buffer)
-    }
-}
-
-impl Decode for C2SLegacyPing {
-    fn decode(buffer: &mut dyn std::io::Read) -> anyhow::Result<Self> {
-        Ok(Self {
-            payload: buffer.read_u8()?,
-        })
-    }
-}
