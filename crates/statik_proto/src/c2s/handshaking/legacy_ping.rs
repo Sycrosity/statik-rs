38aaac6653dc355e15948b6ecf54c13b77344e10
Sycrosity <72102935+Sycrosity@users.noreply.github.com>
2023-05-12 15:54:54 +0100

    packets should have public fields

--- crates/statik_proto/src/c2s/handshaking/legacy_ping.rs
+++ crates/statik_proto/src/c2s/handshaking/legacy_ping.rs
@@ -1,7 +1,33 @@
-use statik_derive::Packet;
+use statik_common::prelude::*;
+// use statik_derive::Packet;
+use byteorder::{ReadBytesExt, WriteBytesExt};
 
 #[derive(Debug)]
 pub struct C2SLegacyPing {
     ///always 1 (0x01).
-    payload: u8,
+    pub payload: u8,
+}
+
+impl Packet for C2SLegacyPing {
+    fn id(&self) -> VarInt {
+        VarInt(0x0F)
+    }
+
+    fn length(&self) -> VarInt {
+        VarInt(2)
+    }
+}
+
+impl Encode for C2SLegacyPing {
+    fn encode(&self, buffer: &mut dyn std::io::Write) -> anyhow::Result<()> {
+        self.payload.encode(buffer)
+    }
+}
+
+impl Decode for C2SLegacyPing {
+    fn decode(buffer: &mut dyn std::io::Read) -> anyhow::Result<Self> {
+        Ok(Self {
+            payload: buffer.read_u8()?,
+        })
+    }
 }
--- crates/statik_proto/src/c2s/status/ping.rs
+++ crates/statik_proto/src/c2s/status/ping.rs
@@ -3,5 +3,5 @@ use statik_derive::Packet;
 #[derive(Debug)]
 pub struct C2SPing {
     ///May be any number. Notchian clients use a system-dependent time value which is counted in milliseconds.
-    payload: i64,
+    pub payload: i64,
 }
