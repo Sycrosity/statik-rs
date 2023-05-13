82a1e7810a30cd598cf307cc8d2bad4e5ea9b161
Sycrosity <72102935+Sycrosity@users.noreply.github.com>
2023-05-13 17:15:54 +0100

    added helper 'PrintTokenSteam' derive macro

--- crates/statik_derive/src/lib.rs
+++ crates/statik_derive/src/lib.rs
@@ -15,7 +15,7 @@ extern crate proc_macro2;
 use proc_macro::TokenStream;
 use syn::{DeriveInput, GenericParam, Generics};
 
-#[proc_macro_derive(Packet, attributes(packet_id))]
+#[proc_macro_derive(Packet, attributes(id))]
 pub fn derive_packet(input: TokenStream) -> TokenStream {
     let mut input = parse_macro_input!(input as syn::DeriveInput);
     packet::expand_derive_packet(&mut input)
@@ -37,4 +37,12 @@ pub fn derive_decode(input: TokenStream) -> TokenStream {
     decode::expand_derive_decode(&mut input)
         .unwrap_or_else(syn::Error::into_compile_error)
         .into()
+}
+
+#[proc_macro_derive(PrintTokenStream)]
+pub fn derive_print_token_stream(input: TokenStream) -> TokenStream {
+    let mut input = parse_macro_input!(input as syn::DeriveInput);
+    println!("{input:#?}");
+
+    TokenStream::new()
 }
